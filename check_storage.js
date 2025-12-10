f93920d94ac5944b48e50ae8ef189a4b2635e2eb
Ranbir Singh <ranbir.singh.live@gmail.com>
2025-12-09 22:32:58 -0500

    Update PRD and Projections page, add assets and utility files

---
+++ check_storage.js
@@ -0,0 +1,5 @@
+// Quick script to check localStorage
+const fs = require('fs');
+console.log('Checking if this is a browser environment...');
+console.log('Note: localStorage is only available in browser, not Node.js');
+console.log('Please check your browser console or DevTools > Application > Local Storage');
